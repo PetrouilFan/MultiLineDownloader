8e1015c056b542e50f0014555a2352bcfb3d98f6
Petrouil <pfanioudakis@gmail.com>
2022-04-20 23:14:30 +0300

    added split in parts feature

--- client.py
+++ client.py
@@ -2,46 +2,123 @@ import socket
 import sys
 import threading
 from time import sleep
+from git import Object
 import psutil
 import os
 import hashlib
+from blessings import Terminal
 
 HOST = os.getenv("mld_host")
 PORT = 10000
 SEPARATOR = "<SEP>"
 BUFFER_SIZE = 1024 * 4 #4KB
+MAX_SPLITTED_PARTS = 20
 
-class Progress_Bar:
-    def __init__(self,total,threads) -> None:
-        self.threads = threads
+class Terminal_Writer(object):
+    def __init__(self, location):
+        self.location = location
+
+    def write(self, string):
+        with term.location(*self.location):
+            print(string)
+
+class part_handler:
+    def __init__(self,filesize, total_ips) -> None:
+        self.filesize = filesize
+        self.total_parts = (filesize//total_ips)//(50*BUFFER_SIZE) # HOW MANY PARTS CAN BE CREATED OF AT LEAST 50 CHUNKS
+        if self.total_parts > MAX_SPLITTED_PARTS:
+            self.total_parts = MAX_SPLITTED_PARTS
+        elif self.total_parts == 0:
+            self.total_parts = 1
+        starts = self.get_file_starts(self.total_parts)
+        ends = self.get_file_ends(self.total_parts)
+        self.chunks = []
+        for x in range(len(starts)):
+            self.chunks.append([starts[x],ends[x]])
+        self.part_nums = list(range(0,self.total_parts))
+
+    def get_file_starts(self,parts):
+        sizes = []
+        _equal_chunks = self.filesize//parts
+        for x in range(parts):
+            sizes.append(_equal_chunks*x)
+        return sizes
+
+    def get_file_ends(self,parts):
+        sizes = self.get_file_starts(parts)
+        sizes.pop(0)
+        for x in range(len(sizes)):
+            sizes[x] -= 1
+        sizes.append(self.filesize-1)
+        return sizes
+
+class Bar(Object):
+    def __init__(self,text,total,location,bar_size=None):
+        self.text = text
         self.total = total
+        if not bar_size:
+            self.get_bar_size = lambda: term.width-len(f"{self.text}: || 100.0 %")-2
+        else:
+            self.get_bar_size = lambda: bar_size
+        self.writer = Terminal_Writer((location[0],term.height-1+location[1]))
+
+    def update(self,value):
+        percentage = round((value/self.total)*(100),1)
+        bar_filled = round(round(percentage) / 100 * self.get_bar_size())
+        bar = '█'*bar_filled + ' '*(self.get_bar_size() - bar_filled)
+        self.writer.write(f"{self.text}: |{bar}| {percentage} %")
+
+class Progress:
+    def __init__(self) -> None:
         self.value = 0
-        self.total_written = 0
-        self.bar_size = 25
+
+    def config(self,total,part_handler):
+        self.total = total
+        self.part_progress_bars = self.Part_Progress(part_handler)
+        self.total_progress_bar = Bar("Progress",total,(0,-1))
+
+    def start(self):
         self.working = True
-        self.thread = threading.Thread(target=self.handler)
+        self.thread = threading.Thread(target=self.thread_handler)
         self.thread.daemon = True
         self.thread.start()
 
     def update(self):
-        percentage = round((self.total_written/(self.total))*(100),1)
-        bar_filled = round(percentage / 100 * self.bar_size)
-        bar = '#'*bar_filled + ' '*(self.bar_size - bar_filled)
-        print(f"|{bar}| {percentage} % Done",end="         \r")
+        self.part_progress_bars.update()
+        self.total_progress_bar.update(self.value)
 
-    def handler(self):
+    def thread_handler(self):
         while self.working:
             self.update()
-            sleep(0.5)
+            sleep(0.2)
 
     def stop(self):
-        bar = '#'*self.bar_size
-        print(f"|{bar}| {100.00} % Done",end="\r")
         self.working = False
 
-class writter:
-    def __init__(self,filename,filesize,total_threads) -> None:
-        self.bar = Progress_Bar(filesize,total_threads)
+    class Part_Progress:
+        def __init__(self,part_handler):
+            self.part_handler = part_handler
+            self.values = [] # bytes done for every part
+            self.parts_bytes_total = []
+            for x in range(self.part_handler.total_parts):
+                self.values.append(0)
+                _chunk_size = self.part_handler.chunks[x][1] - self.part_handler.chunks[x][0]
+                self.parts_bytes_total.append(_chunk_size)
+            
+            # Prepare parts writers
+            print("\n"*(self.part_handler.total_parts))
+            self.part_bars = []
+            for part_num in range(self.part_handler.total_parts):
+                _y = 0 - 1 - (self.part_handler.total_parts - part_num)
+                _text = f"Part{' '*(3-len(str(part_num+1)))}{part_num+1}"
+                self.part_bars.append(Bar(_text,self.parts_bytes_total[part_num],(0,_y),bar_size=15))
+        def update(self):
+            for _bar_num in range(len(self.part_bars)):
+                self.part_bars[_bar_num].update(self.values[_bar_num])
+
+class File_Writer:
+    def __init__(self,filename,filesize,total_threads,bar) -> None:
+        self.bar = bar
         self.filename = filename
         self.buffer = []
         self.stop_bool = False
@@ -59,28 +136,13 @@ class writter:
                     data = self.buffer.pop(0)
                     f.seek(data[0])
                     f.write(data[1])
-                    self.bar.total_written += len(data[1])
+                    self.bar.value += len(data[1])
                 else:
                     sleep(0.0001)
             return
     def close(self):
         self.stop_bool = True
         self.write_thread.join()
-
-def get_file_starts(filesize,threads):
-    sizes = []
-    _equal_chunks = filesize//threads
-    for x in range(threads):
-        sizes.append(_equal_chunks*x)
-    return sizes
-
-def get_file_ends(filesize,threads):
-    sizes = get_file_starts(filesize,threads)
-    sizes.pop(0)
-    for x in range(len(sizes)):
-        sizes[x] -= 1
-    sizes.append(filesize-1)
-    return sizes
 
 def validIP(address):
     parts = address.split(".")
@@ -100,44 +162,40 @@ def get_ip_addresses():
                 NICs.append([interface, address])
     return NICs
 
-def hashfile(filename):
-    openedFile = open(filename,'rb')
-    readFile = openedFile.read()
-    openedFile.close()
-    hash = hashlib.sha256(readFile).hexdigest()
-    print(f"File {filename} Hash: {hash}")
-
-def connection_downloader(filename, thread_num, filesize, file_writter, nic, total_ips):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, 25, nic[0].encode()) # Needed to bind interface in Linux ( Not tested in Windows )
-        # s.bind((nic[1],PORT+thread_num)) # It may need in order to bind interface in Windows
-        s.connect((HOST, PORT+1))
-        _to_send = f"{BUFFER_SIZE}{SEPARATOR}"
-        _to_send += f"{total_ips}{SEPARATOR}"
-        _to_send += f"{thread_num}{SEPARATOR}"
-        _to_send += f"{filename}{SEPARATOR}"
-        _to_send += f"{filesize}{SEPARATOR}"
-        s.sendall(_to_send.encode())
-        start = get_file_starts(filesize,total_ips)[thread_num]
-        end = get_file_ends(filesize,total_ips)[thread_num]
+def connection_downloader(filename, thread_num, filesize, file_writter, nic, total_ips, parts):
+    current_part = 0
+    while len(parts.part_nums) > 0:
+        current_part = parts.part_nums.pop(0)
+        start,end = parts.chunks[current_part]
         cursor = start
-        total_recieved = 0
-        while cursor < end:
-            data = s.recv(BUFFER_SIZE)
-            if len(data) == 0:
-                break
-            file_writter.write(cursor,data)
-            total_recieved += len(data)
-            cursor += len(data)
-        print(f"Thread: {thread_num} Finished         ")
-        return
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, 25, nic[0].encode()) # Needed to bind interface in Linux ( Not tested in Windows )
+            # s.bind((nic[1],PORT+thread_num)) # It may need in order to bind interface in Windows
+            s.connect((HOST, PORT+1))
+            _to_send = f"{BUFFER_SIZE}{SEPARATOR}"
+            _to_send += f"{total_ips}{SEPARATOR}"
+            _to_send += f"{thread_num}{SEPARATOR}"
+            _to_send += f"{filename}{SEPARATOR}"
+            _to_send += f"{filesize}{SEPARATOR}"
+            _to_send += f"{current_part}{SEPARATOR}"
+            s.sendall(_to_send.encode())
+            while cursor < end:
+                data = s.recv(BUFFER_SIZE)
+                if len(data) == 0:
+                    break
+                file_writter.write(cursor,data)
+                bar.part_progress_bars.values[current_part] += len(data)
+                cursor += len(data)
+            s.close()
+        # if end-cursor != -1:
+        #     parts.part_nums.append(current_part) # Redownload chunk if missing data
 
-def multi_connection_handler(filename, filesize, file_writter):
+def multi_connection_handler(filename, filesize, file_writter,parts):
     NICs = get_ip_addresses()
     total_ips = len(NICs)
     threads = []
     for thread_num in range(total_ips):
-        _thread = threading.Thread(target=connection_downloader,args=(filename,thread_num,filesize,file_writter, NICs[thread_num],total_ips))
+        _thread = threading.Thread(target=connection_downloader,args=(filename,thread_num,filesize,file_writter, NICs[thread_num],total_ips, parts))
         threads.append(_thread)
         _thread.daemon = True
         _thread.start()
@@ -156,10 +214,17 @@ def main(URL):
     filesize = int(data[1])
     NICs = get_ip_addresses()
     total_ips = len(NICs)
-    file_writter = writter(original_filename,filesize,total_ips)
-    multi_connection_handler(filename,filesize,file_writter)
+    parts = part_handler(filesize,total_ips)
+    file_writter = File_Writer(original_filename,filesize,total_ips,bar)
+    bar.config(filesize,parts)
+    bar.start()
+    multi_connection_handler(filename,filesize,file_writter,parts)
     file_writter.close()
-    hashfile(original_filename)
+    sleep(0.5)
+    # hashfile(original_filename)
+
+term = Terminal()
+bar = Progress()
 
 if __name__ == '__main__':
     if HOST == None:
@@ -167,4 +232,3 @@ if __name__ == '__main__':
         sys.exit()
     URL = input("URL: ")
     main(URL)
-
--- server.py
+++ server.py
@@ -5,16 +5,41 @@ import sys
 import subprocess
 import hashlib
 from time import sleep
-import hashlib
 
 PORT = 10000
 SEPARATOR = "<SEP>"
 DOWNLOAD_LOCATION = ".tmp\\" # EMPTY FOR THE CURRENT DIRECTORY
+MAX_SPLITTED_PARTS = 20
 
 try:
     os.chdir(os.path.dirname(sys.argv[0]))
 except:
     pass
+
+def part_calculator(filesize,total_threads,current_part,BUFFER_SIZE):
+    total_parts = (filesize//total_threads)//(50*BUFFER_SIZE) # HOW MANY PARTS CAN BE CREATED OF AT LEAST 50 CHUNKS
+    if total_parts > MAX_SPLITTED_PARTS:
+        total_parts = MAX_SPLITTED_PARTS
+    elif total_parts == 0:
+        total_parts = 1
+    starts = get_file_starts(filesize,total_parts)
+    ends = get_file_ends(filesize,total_parts)
+    return starts[current_part],ends[current_part]
+
+def get_file_starts(filesize,parts):
+    sizes = []
+    _equal_chunks = filesize//parts
+    for x in range(parts):
+        sizes.append(_equal_chunks*x)
+    return sizes
+
+def get_file_ends(filesize,parts):
+    sizes = get_file_starts(filesize,parts)
+    sizes.pop(0)
+    for x in range(len(sizes)):
+        sizes[x] -= 1
+    sizes.append(filesize-1)
+    return sizes
 
 def get_file_starts(filesize,parts):
     sizes = []
@@ -67,9 +92,9 @@ def send_handler(conn):
     current_thread = int(data[2])
     filename = data[3]
     filesize = int(data[4])
-    start = get_file_starts(filesize,total_ips)[current_thread]
-    end = get_file_ends(filesize,total_ips)[current_thread]
-    cursor = start*current_thread
+    current_part = int(data[5])
+    start,end = part_calculator(filesize,total_ips,current_part,BUFFER_SIZE)
+    cursor = start
     with open(DOWNLOAD_LOCATION+filename, "rb") as f:
         while cursor <= end:
             f.seek(cursor)
